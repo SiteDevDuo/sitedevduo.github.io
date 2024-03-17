572ac9d97e05b9fb5910814c8bc5a98f2a5a7763
SiteDevDuo <163656952+SiteDevDuo@users.noreply.github.com>
2024-03-18 01:09:05 +0400

    Add files via upload

---
+++ Games/items.js
@@ -0,0 +1,87 @@
+items = `
+Earth (discovered)
+Fire (discovered)
+Water (discovered)
+Air (discovered)
+Soul (discovered)
+Lava
+Steam
+Obsidian
+Mud
+Sea
+Land
+Hot air
+Sparkle
+Explosion
+Nuke
+Fish
+Bird
+Worm
+Demon
+Grand soul
+Corruption
+Death
+Meat
+Life
+Holy Life
+Body
+Atmosphere
+Angel
+Human
+Time
+Dust
+Age
+Oldman
+Cold air
+Wind
+Tornado
+Cloud
+Nether
+Hell
+`
+
+recipes = `
+Lava = Earth + Fire
+Steam = Fire + Water
+Obsidian = Lava + Water
+Mud = Water + Earth
+Sea = Water + Water
+Land = Earth + Earth
+Hot air = Fire + Air
+Sparkle = Fire + Fire
+Explosion = Sparkle + Sparkle
+Nuke = Explosion + Explosion
+Fish = Water + Soul
+Bird = Air + Soul
+Worm = Earth + Soul
+Demon = Fire + Soul
+Devil = Lava + Grand soul
+Grand soul = Soul + Soul
+Steam = Sea + Lava
+Corruption = Grand soul + Devil
+Death = Explosion + Bird
+Death = Explosion + Worm
+Death = Explosion + Fish
+Death = Nuke + Bird
+Death = Nuke + Worm
+Death = Nuke + Fish
+Meat = Death + Bird
+Meat = Death + Worm
+Meat = Death + Fish
+Body = Meat + Meat
+Life = Body + Soul
+Holy Life = Body + Grand soul
+Atmosphere = Air + Air
+Angel = Holy Life + Body
+Human = Life + Body
+Dust = Fire + Time
+Age = Time + Human
+Oldman = Age + Human
+Death = Oldman + Time
+Cold air = Hot air + Time
+Wind = Cold air + Hot air
+Tornado = Wind + Wind
+Cloud = Wind + Steam
+Nether = Obsidian + Fire
+Hell = Devil + Nether
+`
